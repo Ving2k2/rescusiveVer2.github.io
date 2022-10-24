aa4f827bc6ca110c3ea007f9b94fa5b7e022f35b
lacthanh <laanhthu520@gmail.com>
2022-10-24 23:38:11 +0700

    fix dynamic

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-
+    content_page =$(".content-page")[0];
     // change content size
     function changeSize() {
-        $(".content-page").height(($(".footer").height() + $("#main-content").height()) + "px")
+        content_page.height(($(".footer").height() + $("#main-content").height()) + "px")
     }
     $(window).on('resize', function () {
         changeSize()
@@ -11,7 +11,7 @@ $(document).ready(function () {
     var observer = new MutationObserver(function () {
         changeSize()
     });
-    observer.observe($(".content-page")[0], {characterData: true, childList: true, attributes: true, subtree: true})
+    observer.observe(content_page, {characterData: true, childList: true, attributes: true, subtree: true})
 
     $(document).on('click', '.like-button', function () {
         $(this).children("i").toggleClass("text-danger")
@@ -28,6 +28,13 @@ $(document).ready(function () {
     var observer2 = new MutationObserver(function () {
         $('.side-nav').metisMenu('dispose').metisMenu();
     });
-    observer2.observe($('.side-nav')[0], {childList: true})
+    observer2.observe($('.side-nav')[0], {childList: true, subtree: true})
+    var observer3 =new MutationObserver(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+        $('[data-toggle="toast"]').toast();
+        $('[data-toggle="touchspin"]').TouchSpin();
+    });
+    observer3.observe($('body')[0], {childList: true, subtree: true})
 })
 
