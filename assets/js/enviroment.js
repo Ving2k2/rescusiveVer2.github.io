09fbff9b6b405a217ac6cdae68bbfa257da475ca
QuangThang1804 <thangpham1804@gmail.com>
2022-10-27 10:40:38 +0700

    update dè thi

--- assets/js/enviroment.js
+++ assets/js/enviroment.js
@@ -1,5 +1,5 @@
 
-export var url="https://ving2k2.github.io/rescusiveVer2.github.io"
+export var url="http://127.0.0.1:5500/"
 // change when error
 // http://127.0.0.1:5500/
 // https://ving2k2.github.io/rescusiveVer2.github.io
